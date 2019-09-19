b2db131aa121810a48691c8f90183cb93df8c691
CookieMonsta89 <jlstan06@hotmail.com>
2019-09-19 08:33:34 -0400

    ternary readablity

--- src/components/display.js
+++ src/components/display.js
@@ -1,24 +1,31 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
+import React from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
 
 const Display = props => {
-    const {display} = props;
-    return ( 
-        <div className="display" value={display}>
-            {display.length !== 0 ? display.length > 10 ? display.substring(0, 10) + "..." + display.substring(display.split('').length - 4, display.split('').length) : display : '0'}
-        </div>
-     );
-}
+  const { display } = props;
+  return (
+    <div className="display" value={display}>
+      {display.length !== 0
+        ? display.length > 10
+          ? display.substring(0, 10) +
+            "..." +
+            display.substring(
+              display.split("").length - 4,
+              display.split("").length
+            )
+          : display
+        : "0"}
+    </div>
+  );
+};
 
 Display.propTypes = {
-    display: PropTypes.string
-  };
+  display: PropTypes.string
+};
 
-const mapStateToProps = (state) => (
-    {
-      display:state.display
-    }  
-)
- 
+const mapStateToProps = state => ({
+  display: state.display
+});
+
 export default connect(mapStateToProps)(Display);
